61fc9efb0c428bb635c8c04c3e1bda3d518ba358
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-12-28 10:42:15 -0800

    creating custom buttons

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { View, TextInput} from 'react-native';
+
+function PrimaryButton({children}) {
+    return (
+        <View>
+            <Text>{children}</Text>
+        </View>
+    )
+}
+
+export default PrimaryButton;
